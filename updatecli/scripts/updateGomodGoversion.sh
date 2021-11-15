15c103f0aba1baa1ad9bc77e7174ef95c4e295af
Damien Duportal <damien.duportal@gmail.com>
2021-11-15 13:04:00 +0100

    chore(updatecli) fix gh workflow
    - target updatecli apply on staging branch
    - install golang inside the updatecli docker image

--- updatecli/scripts/updateGomodGoversion.sh
+++ updatecli/scripts/updateGomodGoversion.sh
@@ -13,8 +13,17 @@ go_version="${2}"
 new_version="$(echo "${go_version}" | cut -d. -f1,2)"
 tmp_dir="$(mktemp -d)"
 
-## Ensures that the correct golang version is used
+## Ensures that there is a golang version installed
 {
+  if ! command -v go
+  then
+    curl --silent --show-error --location --output "${tmp_dir}/go.tgz" \
+      "https://golang.org/dl/go${go_version}.$(uname -s | tr '[:upper:]' '[:lower:]')-amd64.tar.gz"
+    mkdir -p "${tmp_dir}/.bin"
+    tar xzf "${tmp_dir}/go.tgz" -C "${tmp_dir}/.bin"
+    export PATH="${PATH}":"${tmp_dir}"/.bin/go/bin
+  fi
+
   command -v go
   go version
 } >&2
@@ -22,6 +31,8 @@ tmp_dir="$(mktemp -d)"
 ## Copy go mod's directory to a temp directory an start working from this temp. dir.
 cp -r "${go_mod_dir}"/* "${tmp_dir}" >&2
 cd "${tmp_dir}" >&2
+GOPATH="$(mktemp -d)"
+export GOPATH
 
 ## Update go mod properly
 go mod edit -go="${new_version}" >&2
