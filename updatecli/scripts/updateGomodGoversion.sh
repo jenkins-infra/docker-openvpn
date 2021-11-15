a13e453778c134c6b3dc246f63efcca1a09ac296
Damien Duportal <damien.duportal@gmail.com>
2021-11-15 12:48:45 +0100

    chore(updatecli + gh-actions) enable automatic upgrade of go version and go modules
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ updatecli/scripts/updateGomodGoversion.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+## This script updates the go version of a go mod file and prints the resulting content in the stdout
+## Please note that it will NEVER change the go.mod file (works in a temp directory)
+##
+## Expected arguments:
+## - 1: the path to the go.mod file
+## - 2: the new golang version in semver format (major.minor.patch)
+
+set -eux
+
+go_mod_dir="$(dirname "${1}")"
+go_version="${2}"
+new_version="$(echo "${go_version}" | cut -d. -f1,2)"
+tmp_dir="$(mktemp -d)"
+
+## Ensures that the correct golang version is used
+{
+  command -v go
+  go version
+} >&2
+
+## Copy go mod's directory to a temp directory an start working from this temp. dir.
+cp -r "${go_mod_dir}"/* "${tmp_dir}" >&2
+cd "${tmp_dir}" >&2
+
+## Update go mod properly
+go mod edit -go="${new_version}" >&2
+go mod tidy >&2
+echo "" >> go.mod ## Ad empty endline to be POSIX compliant
+
+## Show new go mod
+cat go.mod
+exit 0
