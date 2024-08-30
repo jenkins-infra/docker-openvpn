36d8e2ffa8571283251bc0a08f05c350a9a29d6c
smerle33 <stephane.merle+jenkins@gmail.com>
2024-08-30 11:08:31 +0200

    WIP

---
+++ updatecli/scripts/datediff.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script calculate diff between dates for letsencrypt expiration
+set -eux -o pipefail
+
+currentexpirydate="${1}"
+DATE_BIN='date'
+
+## non GNU operating system
+if command -v gdate >/dev/null 2>&1
+then
+    DATE_BIN='gdate'
+fi
+command -v "${DATE_BIN}" >/dev/null 2>&1 || { echo "ERROR: ${DATE_BIN} command not found. Exiting."; exit 1; }
+
+currentdateepoch=$("${DATE_BIN}" --utc "+%s" 2>/dev/null)
+expirydateepoch=$("${DATE_BIN}" "+%s" -d "$currentexpirydate")
+
+datediff=$(((expirydateepoch-currentdateepoch)/(60*60*24))) # diff per days
+
+if [ "$datediff" -lt 300 ] # launch renew 30 days before expiration
+then
+    echo "time for update"
+    exit 0
+else
+    echo "not yet expired"
+    exit 1
+fi
